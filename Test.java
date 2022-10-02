03cc9993be509700b052b86aa7120a7d3b9fb196
Lin <412533919@qq.com>
2022-10-02 23:21:15 +0800

    第一次提交

---
+++ Test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Test {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		management m=new management();
+		m.init(10);
+		System.out.print("«Î ‰»Î≤È’“µƒÀﬁ…·∫≈£∫");
+		int room=in.nextInt();
+		in.close();
+		m.search(room);
+	}
+}
