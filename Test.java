e4e9b9c5cc2a7fafe56a8263ce033678937b954f
Lin <412533919@qq.com>
2022-10-02 23:10:00 +0800

    This reverts commit ce0b0d7 (第二次提交, 2022-10-02).

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ public class Test {
 		Scanner in=new Scanner(System.in);
 		management m=new management();
 		m.init(10);
-		System.out.print("ÇëÊäÈë²éÕÒµÄËŞÉáºÅ£º");//×¢ÊÍ
+		System.out.print("ÇëÊäÈë²éÕÒµÄËŞÉáºÅ£º");
 		int room=in.nextInt();
 		in.close();
 		m.search(room);
